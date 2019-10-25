f40202f87220872f32685c059abdf609b7126f29
HZJ <1054919923@qq.com>
2019-10-25 21:49:10 +0800

    update all

---
+++ tool.php
@@ -0,0 +1,13 @@
+<?php
+
+$i = 20;
+while($i--){
+    echo 
+    '<div class="love">
+    <div class="love_horizontal">
+        <div class="love_vertical">
+            <div class="love_word">。</div>
+        </div>
+    </div>
+</div>';
+}
